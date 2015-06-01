e60c4bd21a7e9d2e75a88f0bb97bbe6d6877ef9e
Alljun-Lee <lijj@cloud-times.com>
2015-06-01 06:57:22 -0700

    gluster: gluster_ui_constants.py

---
+++ gluster/gluster_ui_constants.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+# Copyright (C) 2015 Alljun Lee, CT
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Refer to the README and COPYING files for full details of the license
+
+
+GLST_DEFAULT = -1
+
+GLST_WIDTH = 800
+GLST_HEIGHT = 600
+GLST_TEXT_HEIGHT = 20
+GLST_TEXT_WIDTH = 32
+GLST_NOTEBOOK_WIDTH = 800
+GLST_NOTEBOOK_HEIGHT = 600
+GLST_NOTEBOOK_TAB_WIDTH = 200
+GLST_NOTEBOOK_TAB_HEIGHT= 50
+GLST_COLOR_BUTTON_HEIGHT = 30
+GLST_LOG_WIN_WIDTH = 610
+GLST_LOG_WIN_HEIGHT = 340
+GLST_DETAILED_LIST_HEIGHT = 80
+GLST_BUTTON_LIST_HEIGHT = 60
+GLST_COLOR_BUTTON_HEIGHT = 30
+GLST_LOG_WIN_WIDTH = 610
+GLST_LOG_WIN_HEIGHT = 340
+GLST_PAGE_WIDGET_HPADDING = 5
+GLST_PAGE_LISTS_HPADDING = 90
+GLST_ALIGNMENT_CONTENT_X = 0
+GLST_ALIGNMENT_CONTENT_Y = 0.5
+GLST_ALIGNMENT_TITLE_X = 0
+GLST_ALIGNMENT_TITLE_Y = 0.5
+GLST_ALIGNMENT_TITLE_LONG_X = 0
+GLST_ALIGNMENT_TITLE_LONG_Y = GLST_ALIGNMENT_TITLE_Y
+GLST_PADDING_TITLE = 30
+GLST_PADDING_CONTENT_FIRST = 90
+GLST_PADDING_CONTENT_NEXT = 5
+GLST_PADDING_LIST = 90
+
+#Colors for widgets
+GLST_LOG_PAGE_BG = "#34322D"
+GLST_INIT_BTN_BG = "#f9fbfa"
+GLST_INIT_BOX_BG = "#f9fbfa"
+GLST_SELECTED_BTN_BG = "#5c822f"
+GLST_SELECTED_TAB_BG = "#5c822f"
+GLST_LOG_PAGE_BG = "#34322D"
+GLST_HEADER_BG = "#123456"
+GLST_FOOTER_BG = GLST_HEADER_BG
+
+# GTK_RELATED_CONSTANTS
+GTK_SIGNAL_CLICKED = "clicked"
+GTK_SIGNAL_DESTROY = "destroy"
+GTK_SIGNAL_SWITCH_PAGE = "switch-page"
+GTK_SIGNAL_KEY_PRESS = "key_press_event"
+GTK_SIGNAL_CHILD_EXIT = "child-exited"
+GTK_SIGNAL_CLICK_DETAILLIST = "row-activated"
+GTK_SIGNAL_FOCUS_OUT = "focus-out-event"
+
+CmdBase = ['/usr/sbin/gluster']
