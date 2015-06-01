a6be7b812c5ca1f2b494030333124e36919bbc94
Alljun-Lee <lijj@cloud-times.com>
2015-06-01 06:48:45 -0700

    Initialization the header of setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding=utf-8
+#
+# Copyright (C) 2015 Alljun Lee, CT
+#
+# This program is free software; you can redistribute it and/or modify
+# if under the terms of the GUN General Public License as published by 
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Refer to the READMW and COPYING files for full details the license
+
+from distutils.core import setup
+
+setup(
+        name         = "GlusterUi",
+        version      = "1.0",
+        url          = "https://github.com/JiejunLi/Gluster-UI",
+        author       = "alljun Lee",
+        author_email = "lijj@cloud-times.com",
+        packages     = ['glusterui'],
+        license      = 'GPLV2',
+)
